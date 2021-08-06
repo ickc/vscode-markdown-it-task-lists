f2570b32c52dee4c16a74cd5d2427285f111f57a
Kolen Cheung <christian.kolen@gmail.com>
2021-08-06 13:06:30 -0700

    add vscode-markdown-it-task-lists.options

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,4 @@
 # Change Log
 
+- v0.2.0: options configurable via `vscode-markdown-it-task-lists.options` and change default to `enabled: false`
 - v0.1.0: Initial proof of concept
--- extension.js
+++ extension.js
@@ -1,9 +1,14 @@
 'use strict'
 
+const vscode = require('vscode');
+
 function activate(context) {
     return {
         extendMarkdownIt(md) {
-            return md.use(require('markdown-it-task-lists'), {enabled: true});
+            return md.use(
+                require('markdown-it-task-lists'),
+                vscode.workspace.getConfiguration('vscode-markdown-it-task-lists').options
+            );
         }
     };
 }
